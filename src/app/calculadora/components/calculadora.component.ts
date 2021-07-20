0d45ea79d0a8a71ba582810f67bfc245334d49d1
brenomaiacosta <brenomcmaia14@gmail.com>
2021-07-19 23:40:51 -0300

    Projeto Final

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -19,10 +19,6 @@ export class CalculadoraComponent implements OnInit {
     this.limpar();
   }
 
-  /**
-   * Inicializa todos os operadores para os valores padrão.
-   * @return void
-   */
   limpar(): void {
     this.numero1 = '0';
     this.numero2 = null;
@@ -30,11 +26,6 @@ export class CalculadoraComponent implements OnInit {
     this.operacao = null;
   }
 
-  /**
-   * Adiciona o número selecionado para o cálculo posteriormente.
-   * @param string numero
-   * @return void
-   */
   adicionarNumero(numero: string): void {
     if (this.operacao === null) {
       this.numero1 = this.concatenarNumero(this.numero1, numero);
@@ -43,28 +34,49 @@ export class CalculadoraComponent implements OnInit {
     }
   }
 
-  /**
-   * Retorna o valor concatenado. Trata o separador decimal.
-   * @param string numAtual
-   * @param string numConcat
-   * @return string
-   */
   concatenarNumero(numAtual: string, numConcat: string): string {
-    //caso contenha apenas '0' ou null, reinicia o valor
     if (numAtual === '0' || numAtual === null) {
       numAtual = '';
     }
-
-    // primeiro dígito é .', concatena '0' antes do ponto
     if (numConcat === '.' && numAtual === '') {
       return '0. ';
     }
-
-    // caso '.' digitado e já contenha um '.', apenas retorna
     if (numConcat === '.' && numAtual.indexOf('.') > -1) {
       return numAtual;
     }
 
     return numAtual + numConcat;
+  }
+
+  definirOperacao(operacao: string): void {
+    if (this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+    if (this.numero2 !== null) {
+      this.resultado = this.calculadoraService.calcular(parseFloat(this.numero1), parseFloat(this.numero2), this.operacao);
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = null;
+      this.resultado = null;
+    }
+  }
+
+  calcular(): void {
+    if (this.numero2 == null) {
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(parseFloat(this.numero1), parseFloat(this.numero2), this.operacao);
+  }
+
+  get display(): string {
+    if (this.resultado != null) {
+      return this,this.resultado.toString();
+    }
+    if (this.numero2 != null) {
+      return this.numero2;
+    }
+    return this.numero1;
   }
 }
